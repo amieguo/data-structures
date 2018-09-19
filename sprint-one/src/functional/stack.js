0b8c008db904556e1cff02a521996b3ab778b64d
Amie Guo <amieguo@gmail.com>
2018-09-19 11:50:00 -0700

    finished stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count ++;
+    storage[count] = value;
+    return storage[count];
   };
 
   someInstance.pop = function() {
+    count--;
+    if (count < 0) {
+      count = 0;
+    }
+    // delete storage[count];
+    return storage[count+1];
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
+
