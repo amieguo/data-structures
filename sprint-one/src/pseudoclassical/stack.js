6627043b37f1036923655f99331629779d50cf7f
Amie Guo <amieguo@gmail.com>
2018-09-19 21:25:23 -0700

    completed

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+}
 
+Stack.prototype.pop = function() {
+  this.count--;
+  if (this.count < 0) {
+    this.count = 0;
+  }
+  return this.storage[this.count +1];
+}
+
+Stack.prototype.size = function() {
+  return this.count;
+}
+
+var stackB = new Stack();
